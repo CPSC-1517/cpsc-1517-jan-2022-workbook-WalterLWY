6bcc61906734839182a827328cbb7bb9c94bc1e1
Wai Yin Lau <wlau4@studentmail.nait.ca>
2022-02-01 19:33:35 -0700

    Upload Week2-Lesson3-Part1

---
+++ Practice1/src/CSharpGrammar/Practice1/Employment.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice1.Data
+{
+    public class Employment
+    {
+        // An instance of this class will hold data about a person's employment
+        // The code of this class is the definition of that data
+        // The characteristics (data) of the class
+        //   Title, SupervisoryLevel, Year of Employment within the company
+
+        // The 4 components of a class definition are:
+        //   Data fields
+        //   Property
+        //   Constructor
+        //   Behavior (Method)
+
+        //   Data fields
+        //      are storage area in your class
+        //      these are treated as variables
+        //      these may be public, private/ public readonly
+
+
+        //   Property
+        //      These are access techniques to retrieve or set data in
+        //      your class without directly touching the storage data field
+
+        //   Fully-implemented Constructor
+        //      a) a declared storage area (data field)
+        //      b) a declared proparty signature
+        //      c) a coded get "method"
+        //      d) an optional codedd set "method"
+
+
+
+
+        //   Auto-implemented Constructor
+
+
+
+        //   Behavior (Method)
+
+
+
+
+    }
+
+
+}
---
+++ Practice1/src/CSharpGrammar/Practice1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System; 
+/* in .net 6 some using statement visible in .net 5 are 
+   already implemented in the project and do not need 
+   to be explicitly coded*/
+
+/* there are still times that you will still need to code 
+   using statements to explicitly reference other namespace*/
+
+// See https://aka.ms/new-console-template for more information
+DisplayString("Hello World");
+
+static void DisplayString(string text)
+{
+    Console.WriteLine(text);
+}
+

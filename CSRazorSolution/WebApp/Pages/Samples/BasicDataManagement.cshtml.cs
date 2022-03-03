5570bd66407cbb3d2ac91e5aee757ca8e38547d6
Wai Yin Lau <wlau4@studentmail.nait.ca>
2022-03-02 17:25:32 -0700

    Updated up to Feb 17 - Week 7 - Lesson 3 - Part 3

--- CSRazorSolution/WebApp/Pages/Samples/BasicDataManagement.cshtml.cs
+++ CSRazorSolution/WebApp/Pages/Samples/BasicDataManagement.cshtml.cs
@@ -5,8 +5,40 @@ namespace WebApp.Pages.Samples
 {
     public class BasicDataManagementModel : PageModel
     {
+        // create bound properties that can be directly tied to a control on the form
+        // bound properties have the data automatically transferred from the control 
+        //  into the property
+        [BindProperty]
+        public int Num { get; set; } // this is tied to the control use the asp-for
+
+        [BindProperty]
+        public string FavouriteCourse { get; set; }
+
+        [BindProperty]
+        public string Comments { get; set; }
+
+        [TempData]
+        public string Feedback { get; set; }
+
+
         public void OnGet()
         {
+            // execute for a Get request
+            // the first time the page is requested an automatic Get request is sent
+            // execellent "event" to use to do any initialization to your web page display
+            //  as the page is shown for the first time
+        }
+
+        public void OnPost()
+        {
+            // process the OnPost request of the form (method="post")
+            // the returnedtype can be void or IActionResult
+            // OnPost request is the request from a <button> that has NOT indicated a
+            //  specifc process Post using the asp-page-handler
+            // logic that you wish to accomplish should be isolated to the actions
+            //  desired for the button
+            Feedback = $"Number {Num}, Course: {FavouriteCourse}, Comments: {Comments}";
+
         }
     }
 }

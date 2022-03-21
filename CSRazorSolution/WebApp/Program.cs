9ab3f08d51dfb8d93b083d835fdccd4b0d8e7241
Wai Yin Lau <wlau4@studentmail.nait.ca>
2022-03-21 07:47:41 -0600

    Updated up to Mar 7 - Week 9 - Lesson 1 - Part 3

--- CSRazorSolution/WebApp/Program.cs
+++ CSRazorSolution/WebApp/Program.cs
@@ -1,9 +1,29 @@
+#region Additional Namespaces
+using Microsoft.EntityFrameworkCore;
+using WestWindSystem;
+#endregion
+
 var builder = WebApplication.CreateBuilder(args);   // WebApplication = class
                                                     // no new () statement = static class
                                                     // CreateBuilder = method => pass in args argument
                                                     // args = in-built
                                                     // build = instance
+
 // Add services to the container.
+//  Setup the connection string services for the application
+//  (1) Retrieve the connection string information from your appSettings.json
+var connectionString = builder.Configuration.GetConnectionString("WWDB");
+
+//  (2) Register any "services" you wish to use
+//      In our solution, our services will be created (coded) in the class library WestWindSystem
+//      one of these services will be the setup of the database context connection
+//      another services will be created as the application requires
+
+builder.Services.WWBackendDependencies(options => options.UseSqlServer(connectionString));
+
+//  This setup can be done here, locally
+//  This setup can also be done elsewhere and called from this location ****
+
 builder.Services.AddRazorPages();                   // class builder. Services (property) .AddRazorPages() (method)
 
 var app = builder.Build();
---
+++ CSRazorSolution/WestWindSystem/BackendExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using WestWindSystem.DAL;
+#endregion
+
+namespace WestWindSystem
+{
+    public static class BackendExtensions
+    {
+        public static void WWBackendDependencies(this IServiceCollection services,
+            Action<DbContextOptionsBuilder> options)
+        {
+            // Within this method, we will register all the services 
+            // that will be used by the system (context setup)
+            //  and will be provided by the system
+
+            // Setup the context services 
+            services.AddDbContext<WestWindContext>(options);
+
+
+
+        }
+
+
+
+    }
+}

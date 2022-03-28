ef3c8ae87634dd2f1c67a849cdb3c452ad85676d
Wai Yin Lau <wlau4@studentmail.nait.ca>
2022-03-28 02:07:54 -0600

    Updated up to Mar 17 - Week 10 - Lesson 3 - Part 2

--- CSRazorSolution/WebApp/Pages/Samples/RegionQueryOne.cshtml.cs
+++ CSRazorSolution/WebApp/Pages/Samples/RegionQueryOne.cshtml.cs
@@ -34,10 +34,25 @@ namespace WebApp.Pages.Samples
 
         public Region regionInfo { get; set; }
 
+        // The List<T> has a null value as the page is created
+        // You can initialize the property to an instance as the page is
+        //  Being created by adding = new() to your declaration
+        // If you do, you will have an empty instance of List<T>
+        [BindProperty]
+        public List<Region> regionsList { get; set; } = new();
+
+        [BindProperty]
+        public int selectRegion { get; set; }
+
         public void OnGet()
         {
+            // Since the internet is a stateless environment, you need to 
+            //  obtain any list data that is required by your controls or local 
+            //  logic on EVERY instance of the page being processed
+            PopulateLists();
+
             if (regionid > 0)
-            {
+            {              
                 regionInfo = _regionServices.Region_GetbyID(regionid);
                 if (regionInfo == null)
                 {
@@ -49,6 +64,14 @@ namespace WebApp.Pages.Samples
                 }
             }
         }
+
+        private void PopulateLists()
+        {
+            // This method will obtain the data for any require list to be used 
+            //  in populating controls or for local logic
+            regionsList = _regionServices.Region_List();
+        }
+
 
         // Generic falling post handler
         public void OnPost()
@@ -66,6 +89,17 @@ namespace WebApp.Pages.Samples
             // the receiving "regionid" is the routing parameter
             // the sending "regionid" is a BindProperty field
             return RedirectToPage(new { regionid = regionid });
+        }
+
+        public IActionResult OnPostSelect()
+        {
+            if (selectRegion < 1)
+            {
+                FeedbackMessage = "Required: Select a region to view";
+            }
+            // the receiving "regionid" is the routing parameter
+            // the sending "selectRegion" is a BindProperty field
+            return RedirectToPage(new { regionid = selectRegion });
         }
 
         // Specific post method to use in conjunction with asp-page-handler="xxx"
--- CSRazorSolution/WestWindSystem/BLL/RegionServices.cs
+++ CSRazorSolution/WestWindSystem/BLL/RegionServices.cs
@@ -31,12 +31,37 @@ namespace WestWindSystem.BLL
         //  the web page
         // This query will return a single instance of the entity Region (sql table Region)
         //  which matches the incoming argument value
-        public Region Region_GetbyID (int regionID)
+        public Region Region_GetbyID(int regionID)
         {
             Region info = _context.Regions
                             .Where(acollectionrow => acollectionrow.RegionID == regionID)
                             .FirstOrDefault();
             return info;
+        }
+
+        // Get all the records of the spl Region table
+        // Return as a List<T>
+        public List<Region> Region_List()
+        {
+            // Linq querys use two generic collection types
+            //  IQueryable this is a data collection returned from sql
+            //  IEnumerable this is a data collection in local memory
+            // You can convert either of these collections to a List<T> using .ToList()
+            IEnumerable<Region> info = _context.Regions
+                                        .OrderBy(x => x.RegionDescription);
+            return info.ToList();
+
+            // OR 
+
+            // One could convert the returned data collection to a List<T> by 
+            //  placing the conversion method directly on your query
+/*            
+            IEnumerable<Region> info = _context.Regions
+                                        .OrderBy(x => x.RegionDescription)
+                                        .ToList();
+            return info;
+*/
+
         }
 
         #endregion
